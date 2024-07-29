9515419a968b8f25ab11d61e3cb6074fc40df718
pranav <pranav8267@gmail.com>
2024-07-29 16:42:48 +0530

    update auth routes

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,59 +1,10 @@
-import express, { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import User, { IUser } from '../models/User';
+import express from "express";
+import { signinUser, signupUser } from "../controllers/userController";
 
 const router = express.Router();
-const jwtSecret = process.env.JWT_SECRET || 'your_jwt_secret';
 
-router.post('/signup', async (req: Request, res: Response) => {
-    const { name, email, password } = req.body;
-    try {
-        const existingUser: IUser | null = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).send({ message: 'User already exists' });
-          }
-
-          const salt = await bcrypt.genSalt(10);
-          const hashedPassword = await bcrypt.hashSync(password, salt);
-
-          const newUser: IUser = new User({
-            name,
-            email,
-            password: hashedPassword
-          })
-          await newUser.save();
-
-          res.status(201).json({ 
-            message: 'User created successfully',
-            email: newUser.email,
-            user: newUser.name,
-        });
-
-
-    } catch (error) {
-      res.status(500).send({ message: 'Error creating user' });
-    }
-  });
-
-
-  router.post("/signin", async(req: Request, res:Response)=>{
-        const { email, password} = req.body;
-        try {
-            const user: IUser | null = await User.findOne({ email });
-            if (!user) {
-              return res.status(400).send({ message: 'Invalid email or password' });
-            }
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) {
-              return res.status(400).send({ message: 'Invalid email or password' });
-            }
-            const token = jwt.sign({ email: user.email, id: user._id }, jwtSecret, { expiresIn: '1h' });
-            res.send({ token });
-          } catch (error) {
-            res.status(500).send({ message: 'Error signing in' });
-          }
-  })
+router.post('/signup', signupUser);
+router.post("/signin", signinUser)
 
 
 export default router;
