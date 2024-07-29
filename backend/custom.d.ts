ab59776503be0b2b1f9daa9ded75078e48c36f9b
pranav <pranav8267@gmail.com>
2024-07-29 22:58:07 +0530

    add custom type user

---
+++ backend/custom.d.ts
@@ -0,0 +1,10 @@
+import { Request } from 'express';
+import User from './src/models/User';
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: User; 
+        }
+    }
+}
